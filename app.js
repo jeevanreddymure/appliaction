98cb7413a294d7da85ec4d9b66376b46f84a1a28
jeevan mure <jeevanreddymure@gmail.com>
2019-09-29 15:10:48 -0500

    ff

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 function app()
 {
-     const String=(document.querySelector('#Total').value)
-     const String1=(document.querySelector('#rate').value)
-     const Stringm= (document.querySelector('#st1').value)
-     const comp1= string.length()
-     const comp2= string1.length()
-     const comp3= stringm.length()
-     if(comp1>comp2&& comp2<comp3)
+     var String=document.querySelector('#Total').value;
+     var String1=document.querySelector('#rate').value;
+     var Stringm= document.querySelector('#st1').value;
+     var comp1= string.length();
+     var comp2= string1.length();
+     var comp3= stringm.length();
+     if(comp1 > comp2 && comp2<comp3)
      {
-       var w= com2
+       var w= com2;
        }
-  else if(comp3>comp1)
+  else if(comp3 > comp1)
   {
-      var w= comp1
+      var w= comp1;
       }
   else
   {
-    var w = comp3
+    var w = comp3;
     }
 
 
-     document.querySelector('#w').innerHTML=`$${w.toFixed(2)}`
+    var total= document.getElementById('#w').innerHTML= w ;
 
 }
      document.querySelector('#calculate').addEventListener('click',app)
